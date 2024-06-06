4b9ed377c3ce4b969ed85f1a89bf96b25626c87d
Sineth Shashintha <sinethshashintha2000@gmail.com>
2024-06-06 13:57:30 +0530

    selenium link tasks

--- SeleniumPrac/src/test/java/LinkExample.java
+++ SeleniumPrac/src/test/java/LinkExample.java
@@ -7,6 +7,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class LinkExample {
     //declaring webdriver driver to use in any method
     WebDriver driver;
@@ -15,7 +17,7 @@ public class LinkExample {
     public void openLinkTestPage(){
         WebDriver driver = new ChromeDriver();
         //increasing size of the browser to dekstop size
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         driver.get("https://www.leafground.com/link.xhtml;jsessionid=node010nx464jtc8vzxsmrg4fp0yu9138.node0");
     }
 
@@ -25,12 +27,50 @@ public class LinkExample {
 
         WebElement homelink = driver.findElement(By.linkText("Go to Dashboard"));
         homelink.click();
-        driver.navigate().back(); 
 
-//    2--> am i a broken link?
-//    3--> duplicate link
-//    4--> count page links
-//    5--> count layout links
+//        river.findElement(By.linkText("Go to Dashboard")).click();
+        driver.navigate().back();
+
+//    2--> find my destination
+
+        WebElement wheretoGo = driver.findElement(By.partialLinkText("Find the URL without clicking me."));
+        String path = wheretoGo.getAttribute("href");
+        System.out.println("This link is going to:" + path);
+
+//        3 --> am i a broken link?
+
+        WebElement brokenLink = driver.findElement(By.linkText("Broken?"));
+        brokenLink.click();
+
+        String title = driver.getTitle();
+        if (title.contains("404")){
+            System.out.println("The link is broken");
+        }else{
+            System.out.println("Not broken ");
+        }
+
+        driver.navigate().back();
+
+
+//    4--> duplicate link
+
+        WebElement homelink1 = driver.findElement(By.linkText("Go to Dashboard"));
+        homelink1.click();
+        driver.navigate().back();
+
+
+//    5--> count page links
+
+        List<WebElement> countpagelinks = driver.findElements(By.tagName("a"));
+        int pageLinkConut = countpagelinks.size();
+        System.out.println("Count full page links:" + pageLinkConut);
+
+
+//    6--> count layout links
+
+        WebElement layoutElement = driver.findElement(By.className("layout-main-content"));
+        List<WebElement> countofLayoutElemtns = layoutElement.findElements(By.tagName("a"));
+        System.out.println("Count of layout elements:" + countofLayoutElemtns.size() );
     }
 
 
